a3fe6c509a1b98086040dd1742e73aab0795efc8
Axell <axellsolisrubio@gmail.com>
2021-04-20 21:07:25 -0500

    Added navigation trogh the gallery with arrow buttons

--- js/app.js
+++ js/app.js
@@ -5,23 +5,61 @@ const gallery = (() => {
 		imageView = document.querySelector(".img-container"),
 		imageToShow = document.querySelector(".img-show"),
 		imageDescription = document.querySelector(".img-description"),
+		btnNetx = document.querySelector(".nextBtn"),
+		btnBack = document.querySelector(".backBtn"),
+		btnExit = document.querySelector(".exit-icon"),
 		MOVE_CLASS = "move";
 
-	images.forEach((img) => {
+	let currentImageIndex = 0;
+
+	images.forEach((img, index) => {
 		img.addEventListener("click", () => {
+			imageView.classList.toggle(MOVE_CLASS);
+			imageToShow.classList.toggle("img-scale");
+			currentImageIndex = index;
 			addImage(img.src, img.alt);
 		});
 	});
 
-	imageView.addEventListener("click", () => {
+	btnExit.addEventListener("click", () => {
 		imageView.classList.toggle(MOVE_CLASS);
 		imageToShow.classList.toggle("img-scale");
 	});
 
 	const addImage = (src, alt) => {
-		imageView.classList.toggle(MOVE_CLASS);
-		imageToShow.classList.toggle("img-scale");
 		imageToShow.src = src;
 		imageDescription.innerHTML = alt;
+		hideGalleryButtons();
+	};
+
+	btnNetx.addEventListener("click", () => {
+		if (currentImageIndex < images.length - 1) {
+			currentImageIndex++;
+			addImage(images[currentImageIndex].src, images[currentImageIndex].alt);
+			hideGalleryButtons();
+		}
+	});
+
+	btnBack.addEventListener("click", () => {
+		if (currentImageIndex > 0) {
+			currentImageIndex--;
+			addImage(images[currentImageIndex].src, images[currentImageIndex].alt);
+			hideGalleryButtons();
+		}
+	});
+
+	const hideGalleryButtons = () => {
+		switch (currentImageIndex) {
+			case 0:
+				btnBack.classList.add("hideBtn");
+				break;
+			case images.length - 1:
+				btnNetx.classList.add("hideBtn");
+				break;
+			default:
+				btnNetx.classList.remove("hideBtn");
+				btnBack.classList.remove("hideBtn");
+				break;
+		}
 	};
 })();
